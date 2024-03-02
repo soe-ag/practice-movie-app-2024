8b046b95e9dc36ca5eb687a2c68c368cd57bc457
wind-ik <soeag.st@gmail.com>
2024-03-02 09:59:01 +0900

    added uno css

--- app.vue
+++ app.vue
@@ -1,14 +1,11 @@
-<script setup >
-import { NuxtLink } from '#build/components';
-</script>
+<script setup lang="ts"></script>
 
 <template>
-<div>
-  <nav>
-    <NuxtLink to="/">Home</NuxtLink>
-    <NuxtLink to="/movies">Movies</NuxtLink>
-  </nav>
-  <NuxtPage />
+  <div>
+    <nav>
+      <NuxtLink to="/">Home</NuxtLink>
+      <NuxtLink to="/movies">Movies</NuxtLink>
+    </nav>
+    <NuxtPage />
   </div>
 </template>
-
