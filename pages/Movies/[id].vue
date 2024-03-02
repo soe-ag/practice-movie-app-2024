a7716d56290c5279c6a9d0b3552a8b6d887f2a88
wind-ik <soeag.st@gmail.com>
2024-03-02 11:42:24 +0900

    add movie search api

---
+++ pages/Movies/[id].vue
@@ -0,0 +1,20 @@
+<script setup>
+const route = useRoute();
+
+const { data } = await useAsyncData(() => {
+  return $fetch(`http://www.omdbapi.com/?apikey=fbd9f84b&i=${route.params.id}`);
+});
+</script>
+
+<template>
+  <div>
+    <p>id is {{ route.params.id }}</p>
+    <p>here in id page</p>
+
+    <div>
+      <!-- <pre>{{ data }}</pre> -->
+      <NuxtImg :src="data.Poster" />
+      <p>{{ data.Title }} / {{ data.Year }}</p>
+    </div>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,9 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+// http://www.omdbapi.com/?i=tt3896198&apikey=fbd9f84b
+</script>
 
 <template>
-  <div>
-    Page: HomePage
-  </div>
+  <div>Page: HomePage</div>
 </template>
 
 <style scoped></style>
